b9bf3853fd175c168f4ab7d29ece5a3e3b3c5a8d
Walid.al.ashik <walid.al.ashik@gmail.com>
2019-02-23 00:15:37 +0600

    Design

--- app/src/main/java/com/appkwan/exertion/feature/accountchoice/AccontChoiceActivity.java
+++ app/src/main/java/com/appkwan/exertion/feature/accountchoice/AccontChoiceActivity.java
@@ -1,15 +1,25 @@
 package com.appkwan.exertion.feature.accountchoice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
 import com.appkwan.exertion.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class AccontChoiceActivity extends AppCompatActivity {
+
+    @BindView(R.id.mStudentLinearLayout)
+    LinearLayout mStudentLinearLayout;
+    @BindView(R.id.mGuardianLinearLayout)
+    LinearLayout mGuardianLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accont_choice);
+        ButterKnife.bind(this);
     }
 }
